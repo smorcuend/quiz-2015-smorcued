2e09086b6873e1d50e6540c0d5e9fb7f6718feb6
Sergio Morcuende <sergio.morcuende@bq.com>
2015-07-30 20:06:23 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -29,7 +29,7 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname, 'quiz');
-var Quiz = sequelize.import(quiz_path)
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
@@ -38,10 +38,15 @@ sequelize.sync().then(function() {
     // success(..) ejecuta el manejador una vez creada la tabla
     Quiz.count().then(function(count) {
         if (count === 0) { // la tabla se inicializa solo si está vacía
-            return Quiz.create({
-                pregunta: 'Capital de Italia',
-                respuesta: 'Roma'
-            }).then(function() {
+            Quiz.bulkCreate(
+                [{
+                    pregunta: 'Capital de Italia',
+                    respuesta: 'Roma'
+                }, {
+                    pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
+                }]
+            ).then(function() {
                 console.log('Base de datos inicializada')
             });
         };
