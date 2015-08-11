88d3bbca0bc19a4d8e256b2b1265061dbe3ed931
Sergio Morcuende <sergio.morcuende@bq.com>
2015-08-11 21:59:53 +0200

    Autenticacion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -26,6 +27,11 @@ router.get('/author', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
 
+// Definición de rutas de sesion
+router.get('/login', sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -37,5 +43,6 @@ router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
 
 module.exports = router;
